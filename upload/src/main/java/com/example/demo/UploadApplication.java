23d93993357fb1fc9b98522c83ec989f80b5b769
urmilaparmarsingh <33140889+urmilaparmarsingh@users.noreply.github.com>
2017-10-26 22:57:15 -0400

    Add files via upload

---
+++ upload/src/main/java/com/example/demo/UploadApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+/**
+ * 
+ * @author UrmilaParmar
+ *
+ *
+ * This is the main Spring Boot application class. 
+ *
+ */
+ 
+@SpringBootApplication
+@EnableMongoRepositories
+public class UploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadApplication.class, args);
+	}
+}
